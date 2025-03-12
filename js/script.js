cfdf88ae917805facbb95e8e0054fb1cb39f8fd6
Sudip <sudipdasghosh64@gmail.com>
2025-03-12 14:11:27 +0530

    url customize

--- js/script.js
+++ js/script.js
@@ -7,3 +7,7 @@ navButton.addEventListener('click', ()=> {
     nav.style.opacity = '0%';
 });
 
+// .html থাকলে সরিয়ে ফেলবে
+if (window.location.pathname.endsWith(".html")) {
+    window.history.replaceState(null, "", window.location.pathname.replace(".html", ""));
+}
